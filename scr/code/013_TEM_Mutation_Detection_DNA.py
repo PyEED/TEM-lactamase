54606d453c5362739f0ec177cc89e80af301a63c
Niklas Abraham GPU <st184432@stud.uni-stuttgart.de>
2025-03-26 16:58:21 +0000

    update

--- scr/code/013_TEM_Mutation_Detection_DNA.py
+++ scr/code/013_TEM_Mutation_Detection_DNA.py
@@ -65,13 +65,11 @@ for single_tem in result:
 
 # max number of neighbours
 n_neighbours = 100
-name_of_standard_numbering_tool = "standard_numbering_pairwise_blaTEM1a_DNA_KY739721.1"
 
 et = EmbeddingTool()
-sn_dna = StandardNumberingTool(name=name_of_standard_numbering_tool)
 md = MutationDetection()
 
-blaTEM1a_id = "AAB59737.1"
+blaTEM1a_id = "AAB59737.1"  # Lohey
 blaTEM1a_database_id = None
 
 
@@ -90,29 +88,53 @@ print(blaTEM1a_database_id)
 
 
 if __name__ == "__main__":
-    # apply the standard numbering tool to the DNAs that are connected to the blaTEM1a protein
-    query = f"""
-    MATCH (p:Protein {{accession_id: '{blaTEM1a_database_id}'}})-[*1..1]-(d:DNA) RETURN d
+    key_base_sequence_id = "AL513383.1"
+    key_base_sequence_Region_id = 10228113
+
+    name_of_standard_numbering_tool = (
+        "standard_numbering_pairwise_blaTEM1a_DNA_region_10228113"
+    )
+
+    sn_dna = StandardNumberingTool(name=name_of_standard_numbering_tool)
+
+    query_of_standard_numbering_tool_dna_regions_ids = """
+    MATCH (p:Protein {accession_id: $accession_id_blaTEM1a})-[*1..1]-(d:DNA)-[rel:HAS_REGION]->(r:Region)
+    WHERE r.annotation = $region_annotation AND r.sequence_id = $accession_id_blaTEM1a
+    RETURN id(r), d.accession_id
     """
 
-    result = eedb.db.execute_read(query)
+    result_dna_accession_ids = eedb.db.execute_read(
+        query_of_standard_numbering_tool_dna_regions_ids,
+        parameters={
+            "accession_id_blaTEM1a": blaTEM1a_database_id,
+            "region_annotation": "coding sequence",
+        },
+    )
 
-    result_dna_accession_ids = []
-    for single_dna in result:
-        result_dna_accession_ids.append(single_dna["d"]["accession_id"])
+    result_dna_accession_ids_accession_ids = [
+        result_dna_accession_ids[i]["d.accession_id"]
+        for i in range(len(result_dna_accession_ids))
+    ]
 
-    print(result_dna_accession_ids)
+    result_region_ids = [
+        result_dna_accession_ids[i]["id(r)"]
+        for i in range(len(result_dna_accession_ids))
+    ]
+
+    print(result_dna_accession_ids_accession_ids)
+    print(result_region_ids)
 
     # apply the standard numbering tool to the DNAs
     sn_dna.apply_standard_numbering_pairwise(
-        base_sequence_id="KY739721.1",
+        base_sequence_id=key_base_sequence_id,
         db=eedb.db,
-        list_of_seq_ids=result_dna_accession_ids,
+        list_of_seq_ids=result_dna_accession_ids_accession_ids,
         node_type="DNA",
+        region_ids_neo4j=result_region_ids,
     )
 
     # apply the mutation detection tool to the DNAs
-    pairs = [("KY739721.1", i) for i in result_dna_accession_ids]
+    pairs = [(key_base_sequence_id, i) for i in result_dna_accession_ids_accession_ids]
     for pair in pairs:
         md.get_mutations_between_sequences(
             sequence_id1=pair[0],
@@ -120,4 +142,68 @@ if __name__ == "__main__":
             db=eedb.db,
             node_type="DNA",
             standard_numbering_tool_name=name_of_standard_numbering_tool,
+            region_ids_neo4j=result_region_ids,
         )
+
+    # find all Protein and their DNA which are not yet connected through a region region mutation to the base sequence DNA
+    # the idea is that the protein are already connected and now I want to find the dna to the proteins whoch are already connected and get their region ids
+
+    # The previous query might return duplicate entries because:
+    # 1. Multiple paths between proteins
+    # 2. Multiple regions for the same DNA
+    # Using DISTINCT to ensure unique region and DNA combinations
+    query_of_standard_numbering_tool_dna_regions_ids = """
+    MATCH (r1:Region)-[:HAS_REGION]-(d1:DNA)-[:ENCODES]-(p1:Protein)-[:MUTATION]-(p2:Protein)-[:ENCODES]-(d2:DNA)-[:HAS_REGION]-(r2:Region)
+    WHERE p1.accession_id = $accession_id_blaTEM1a 
+      AND r2.sequence_id = p2.accession_id 
+      AND r1.annotation = $region_annotation 
+      AND r2.annotation = $region_annotation
+      AND NOT (r1)-[:MUTATION]-(r2)
+    RETURN DISTINCT id(r2), d2.accession_id
+    """
+
+    result_not_yet_mutated_regions = eedb.db.execute_read(
+        query_of_standard_numbering_tool_dna_regions_ids,
+        parameters={
+            "accession_id_blaTEM1a": blaTEM1a_database_id,
+            "region_annotation": "coding sequence",
+        },
+    )
+
+    region_ids = [
+        result_not_yet_mutated_regions[i]["id(r2)"]
+        for i in range(len(result_not_yet_mutated_regions))
+    ]
+    dna_accession_ids = [
+        result_not_yet_mutated_regions[i]["d2.accession_id"]
+        for i in range(len(result_not_yet_mutated_regions))
+    ]
+
+    print(len(region_ids))  # 4801
+
+    batch_size = 10
+    # in batches of 1000 apply these region ids to the standard numbering tool and detect the mutations
+    for i in range(0, len(region_ids), batch_size):
+        sn_dna.apply_standard_numbering_pairwise(
+            base_sequence_id=key_base_sequence_id,
+            db=eedb.db,
+            list_of_seq_ids=dna_accession_ids[i : i + batch_size],
+            node_type="DNA",
+            region_ids_neo4j=region_ids[i : i + batch_size]
+            + [key_base_sequence_Region_id],
+        )
+
+        # apply the mutation detection tool to the DNAs
+        pairs = [
+            (key_base_sequence_id, i) for i in dna_accession_ids[i : i + batch_size]
+        ]
+        for pair in pairs:
+            md.get_mutations_between_sequences(
+                sequence_id1=pair[0],
+                sequence_id2=pair[1],
+                db=eedb.db,
+                node_type="DNA",
+                standard_numbering_tool_name=name_of_standard_numbering_tool,
+                region_ids_neo4j=region_ids[i : i + batch_size]
+                + [key_base_sequence_Region_id],
+            )
